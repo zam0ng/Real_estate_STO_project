7f2cd61b845d49f586d0c3b4f6276ef1bb3f3266
killthecardz <alsdn8055@naver.com>
2023-11-09 20:50:06 +0900

    tailwindcss removed

--- userfrontend/src/components/kakaoMap.tsx
+++ userfrontend/src/components/kakaoMap.tsx
@@ -4,7 +4,7 @@ import React from "react"
 
 export default function KakaoMap () {
     return(
-        <div id="map" className="w-4/5 h-60 border-2 border-black">
+        <div id="map" className="w-4/5 h-96 border-2 border-black">
             
         </div>
     )
