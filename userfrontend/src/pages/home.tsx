c61b753e4deefc6ca10d17f984fcd0d1c9682bf8
killthecardz <alsdn8055@naver.com>
2023-11-10 17:37:53 +0900

    slider without library finished

--- userfrontend/src/pages/home.tsx
+++ userfrontend/src/pages/home.tsx
@@ -1,11 +1,11 @@
 import KakaoMap from "../components/kakaoMap"
 import ProgressBar from "../components/progressBar"
+import Slider from "../components/slider"
 
 export default function Home () {
     return(
         <div className="flex flex-col items-center  ">
-            <KakaoMap addressKor ="경기도 시흥시 복지로 15 " />
-            <ProgressBar percent={30}/>
+            <Slider />
         </div>
         
     )
