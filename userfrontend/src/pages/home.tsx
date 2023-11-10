bce0fdcec9985e3fb8240d0c262a53c25b6248ec
killthecardz <alsdn8055@naver.com>
2023-11-10 14:10:17 +0900

    kakaomap component finished

--- userfrontend/src/pages/home.tsx
+++ userfrontend/src/pages/home.tsx
@@ -1,16 +1,11 @@
-import ProgressBar from "../components/progressBar"
 import KakaoMap from "../components/kakaoMap"
-
+import ProgressBar from "../components/progressBar"
 
 export default function Home () {
     return(
         <div className="flex flex-col items-center  ">
-            <KakaoMap />
-            <KakaoMap />
-            <KakaoMap />
-            <KakaoMap />
-            <KakaoMap />
-            <KakaoMap />
+            <KakaoMap addressKor ="경기도 시흥시 복지로 15 " />
+            <ProgressBar percent={30}/>
         </div>
         
     )
