79eb2e074d504777ff75dd42e04b153f83590bc0
ojhhh <winnel@naver.com>
2023-11-12 23:02:17 +0900

    마이페이지 기능 정리

--- backend/controllers/mypage/index.ts
+++ backend/controllers/mypage/index.ts
@@ -1,1 +1,109 @@
 import { Request, Response } from "express";
+import { db } from "../../models";
+
+// 입금하기
+export const deposit = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+// 출금하기
+export const drawal = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 입금액 보여주기
+export const depositList = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 출금액 보여주기
+export const drawalList = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 입출금 내역 보여주기
+export const transactionList = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 총 손익 보여주기
+export const sumProfitLost = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 총 매입 보여주기
+export const totalPurchase = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 총 평가 보여주기
+export const avgPurchase = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 종목별 자산 정보
+export const assetInformation = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 배당금
+export const dividendList = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 내 투표 목록
+export const voteList = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 내 청약 목록
+export const subscriptionList = async (req: Request, res: Response) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
--- backend/routers/mypage/index.ts
+++ backend/routers/mypage/index.ts
@@ -1,5 +1,43 @@
 import { Router } from "express";
+import {
+  deposit,
+  drawal,
+  depositList,
+  drawalList,
+  transactionList,
+  sumProfitLost,
+  totalPurchase,
+  avgPurchase,
+  assetInformation,
+  dividendList,
+  voteList,
+  subscriptionList,
+} from "../../controllers/mypage";
 
 const router = Router();
+
+router.post("/deposit", deposit);
+router.post("/drawal", drawal);
+
+// 입금액 보여주기
+router.get("/deposit", depositList);
+// 출금액 보여주기
+router.get("/drawal", drawalList);
+// 입출금 내역 보여주기
+router.get("/transaction_list", transactionList);
+// 총 손익 보여주기
+router.get("/sum_profit_lost", sumProfitLost);
+// 총 매입 보여주기
+router.get("/total_purchase", totalPurchase);
+// 총 평가 보여주기
+router.get("/avg_purchase", avgPurchase);
+// 종목별 자산 정보
+router.get("/asset_information", assetInformation);
+// 배당금
+router.get("/dividend_list", dividendList);
+// 내 투표 목록
+router.get("/vote_list", voteList);
+// 내 청약 목록
+router.get("/subscription_list", subscriptionList);
 
 export default router;
