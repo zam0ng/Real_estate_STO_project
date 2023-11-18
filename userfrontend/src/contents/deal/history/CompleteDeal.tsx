1145eec278aadba7d138202b8fae0f54f019166a
byungjoo0806 <andybyungjoopark@gmail.com>
2023-11-18 16:12:02 +0900

    1118 deal page minor error fix

--- userfrontend/src/contents/deal/history/CompleteDeal.tsx
+++ userfrontend/src/contents/deal/history/CompleteDeal.tsx
@@ -1,45 +1,23 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { serverurl } from '../../../components/serverurl';
 import { useLocation } from 'react-router-dom';
 import { useQuery } from 'react-query';
 
 interface CompleteDealRequest {
-    order_type: string;
+    buyer_order_email: string;
     createdAt: string;
-    price: number;
-    amount: number;
+    order_type: string;
+    seller_order_email: string;
+    trade_amount: number;
+    trade_price: number;
 }
 
 const CompleteDeal: React.FC = () => {
     const currentPage = useLocation();
 
-    const completeList = [
-        {
-            order_type: "판매",
-            createdAt: "2023-11-01 09:00",
-            price: 4000,
-            amount: 5
-        },
-        {
-            order_type: "구매",
-            createdAt: "2023-11-01 10:00",
-            price: 4000,
-            amount: 5
-        },
-        {
-            order_type: "구매",
-            createdAt: "2023-11-01 11:00",
-            price: 4000,
-            amount: 5
-        },
-        {
-            order_type: "판매",
-            createdAt: "2023-11-01 12:00",
-            price: 4000,
-            amount: 5
-        }
-    ];
+    const [orderType,setOrderType] = useState<string>("");
+    const [orderDate,setOrderDate] = useState<string>("");
 
     const fetchCompleteDeal = async (): Promise<CompleteDealRequest[]> => {
         const {data} = await axios.get(`${serverurl}/order/conclusion/${currentPage.state.propertyName}`);
@@ -53,7 +31,18 @@ const CompleteDeal: React.FC = () => {
 
     useEffect(()=>{
         console.log(data);
-    },[data])
+        if(data){
+            data.map((item,index)=>{
+                if(item.order_type === "buy"){
+                    setOrderType("구매");
+                }else{
+                    setOrderType("판매");
+                };
+
+                setOrderDate(item.createdAt.slice(0,10)+ " " + item.createdAt.slice(11,16));
+            })
+        }
+    },[data]);
 
     const fromRecent = data && data.sort((a,b)=>{
         const dateA = new Date(a.createdAt);
@@ -68,19 +57,21 @@ const CompleteDeal: React.FC = () => {
             {fromRecent && fromRecent.map((item,index)=>{
                 return(
                     <div className='w-full h-[30%] flex flex-col items-center text-sm mt-2 mb-2' key={index}>
-                        <div className={`w-[80%] h-1/5 text-xs md:text-lg flex justify-start items-center ${item.order_type === "판매" ? "blueText" : "redText"}`}>{item.order_type}</div>
-                        <div className='w-[80%] h-1/5 text-xxs md:text-sm text-slate-400 flex items-center '>{item.createdAt}</div>
+                        <div className={`w-[80%] h-1/5 text-xs md:text-lg flex justify-start items-center ${orderType === "판매" ? "blueText" : "redText"}`}>
+                            {orderType}
+                        </div>
+                        <div className='w-[80%] h-1/5 text-xxs md:text-sm text-slate-400 flex items-center '>{orderDate}</div>
                         <div className='w-[80%] h-1/5 text-xs md:text-sm flex flex-row justify-between'>
                             <p>가격</p>
-                            <div>{item.price} 원</div>
+                            <div>{item.trade_price} 원</div>
                         </div>
                         <div className='w-[80%] h-1/5 text-xs md:text-sm flex flex-row justify-between'>
                             <p>수량</p>
-                            <div>{item.amount} 개</div>
+                            <div>{item.trade_amount} 개</div>
                         </div>
                         <div className='w-[80%] h-1/5 text-xs md:text-sm flex flex-row justify-between'>
                             <p>금액</p>
-                            <div>{item.price * item.amount} 원</div>
+                            <div>{item.trade_price * item.trade_amount} 원</div>
                         </div>
                     </div>
                 )
