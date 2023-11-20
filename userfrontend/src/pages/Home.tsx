c6bb4997a2e9372c6c6101b23f387b70d8823c9a
ojhhh <winnel@naver.com>
2023-11-20 13:37:01 +0900

    tabBar 대소문자 변경 -> TabBar

--- userfrontend/src/pages/Home.tsx
+++ userfrontend/src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import TabBar from "../layouts/tabBar"
+import TabBar from "../layouts/TabBar";
 
-export default function Home () {
-    return(
-        <>
-            <div>홈 화면</div>
-            <TabBar />
-        </>
-        
-    )
+export default function Home() {
+  return (
+    <>
+      <div>홈 화면</div>
+      <TabBar />
+    </>
+  );
 }
