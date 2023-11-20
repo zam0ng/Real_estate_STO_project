bdc60c03e3365116df75ef5d39633240b341e114
ojhhh <winnel@naver.com>
2023-11-20 11:29:07 +0900

    민우꺼랑 머지하기 전 일단 커밋

---
+++ backend/middleware/isLogin/index.ts
@@ -0,0 +1,13 @@
+import { Response, Request, NextFunction } from "express";
+import axios from "axios";
+
+export const isLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
