e1c843de23d603e56b5a06be21f5f14aa5ceecdf
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2023-11-22 15:23:43 +0900

    데이터 없을 때, 물음표 추가

--- adminfrontend/src/app/_contents/admin/real_estates/TableContent.tsx
+++ adminfrontend/src/app/_contents/admin/real_estates/TableContent.tsx
@@ -7,15 +7,16 @@ import { EstateDataItem } from "@/app/_features/admin/real_estates";
 import Link from "next/link";
 
 const TableContent = async () => {
-  
   const estateList = await getEstateList();
-  
+
   return (
     <>
       {/* <div className="grid gap-2 pb-12 pl-12 pr-12 text-base tracking-tight text-center bg-white border-b-2 w-4/4 rounded-b-3xl justify-items-center grid-cols-table mx-44 h-36.5rem  overflow-y-auto		"> */}
       <div className="grid gap-2 pb-12 pl-12 pr-12 text-base tracking-tight text-center bg-white border-b-2 w-4/4 rounded-b-3xl justify-items-center grid-cols-table mx-44">
         {/* 구분선 */}
-        <div className="w-full col-span-9 border-t-4 border-collapse border-neutral-100 ">    </div>
+        <div className="w-full col-span-9 border-t-4 border-collapse border-neutral-100 ">
+          {" "}
+        </div>
 
         {/* 구분 요소 나열 */}
         <TableColumnName columnName={"매물명"} />
@@ -28,7 +29,7 @@ const TableContent = async () => {
         <TableColumnName columnName={"청약 발표일"} />
         <TableColumnName columnName={"STO 발행"} />
 
-        {estateList.map((item: EstateDataItem) => {
+        {estateList?.map((item: EstateDataItem) => {
           return <TableRow key={item.id} item={item} />;
         })}
 
