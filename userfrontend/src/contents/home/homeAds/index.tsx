b0377628ebb322e836d07027b486a575a2f84dd5
killthecardz <alsdn8055@naver.com>
2023-11-26 19:09:22 +0900

    homepage file moved to another dierectory

--- userfrontend/src/contents/home/homeAds/index.tsx
+++ userfrontend/src/contents/home/homeAds/index.tsx
@@ -6,19 +6,12 @@ import HomeIndices from "../homeIndices";
 
 export default function HomeAds(){
 
-    let tab = [{
-        tabName : "오늘 뭐사지?",
-        content : <HomeIndices />
-    },{
-        tabName : "공시 자료",
-        content : <>공시자료 화면</>
-    }]
+
     
     return(
         <div className="mb-16">
             <WalletConnect />
             <SubscriptionAd />
-            <LineTypeTabComponent data={tab}/>
         </div>
     )
 
--- userfrontend/src/pages/Home.tsx
+++ userfrontend/src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import TabBar from "../layouts/TabBar";
 import HomeAds from "../contents/home/homeAds";
+import HomeIndices from "../contents/home/homeIndices";
+import LineTypeTabComponent from "../components/tabUI/LineTypeTabComponent";
+
 
 export default function Home() {
+    let tab = [{
+      tabName : "오늘 뭐사지?",
+      content : <HomeIndices />
+  },{
+      tabName : "공시 자료",
+      content : <>공시자료 화면</>
+  }]
   return (
     <>
       <HomeAds/>
+      <LineTypeTabComponent data={tab}/>
       <TabBar />
     </>
   );
