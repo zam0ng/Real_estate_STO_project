e81f9ebee4b02b98df5171db241b67c0e2573b4b
killthecardz <alsdn8055@naver.com>
2023-12-01 21:00:39 +0900

    오늘 뭐사지 to date

--- userfrontend/src/contents/home/homeIndices/index.tsx
+++ userfrontend/src/contents/home/homeIndices/index.tsx
@@ -10,12 +10,17 @@ export default function HomeIndices(){
         AOS.init({duration : 1200})
     },[])
 
+    const year = new Date().getFullYear().toString();
+    const month = Number(new Date().getMonth().toString().padStart(2,'0'))+1;
+    const date = new Date().getDate().toString().padStart(2,'0');
+
+
 
     return(
     
     <>
-        <div className="w-5/6 m-auto  text-2xl font-extrabold my-10 " data-aos='slide-right' >
-            <div className=" w-5/6 border-b-4 pb-2   ">오늘 뭐사지?</div>
+        <div className="w-5/6 m-auto  text-2xl font-extrabold my-10 text-sm" data-aos='slide-right' >
+            <div className=" w-5/6 border-b-4 pb-2   ">{year}년 {month}월 {date}일</div>
         </div>
         <div className="">
             <div className="w-5/6 m-auto text-xl font-bold ">주요 지수</div>
