882e9ce2e18c73a0a3d6b0de5f0ffe60f06189c5
byungjoo0806 <andybyungjoopark@gmail.com>
2023-12-02 18:42:48 +0900

    1202 market detail fix

--- userfrontend/src/contents/market_detail/property_map/PropertyLocation.tsx
+++ userfrontend/src/contents/market_detail/property_map/PropertyLocation.tsx
@@ -4,10 +4,9 @@ import PropertyMap from './PropertyMap';
 
 const PropertyLocation: React.FC = () => {
   return (
-    <div className='w-full h-96'>
+    <div className='w-full h-80'>
       <div className='w-full h-6 mt-5 ml-5'>주변 살펴보기</div>
       <PropertyMap />
-
     </div>
   )
 }
