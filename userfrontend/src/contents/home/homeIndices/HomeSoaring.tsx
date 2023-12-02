f7872fb13c4246c5055b228b6094e44b14c89181
killthecardz <alsdn8055@naver.com>
2023-12-02 16:15:23 +0900

    chart fluctuation_rate implemented

--- userfrontend/src/contents/home/homeIndices/HomeSoaring.tsx
+++ userfrontend/src/contents/home/homeIndices/HomeSoaring.tsx
@@ -57,10 +57,20 @@ export default function HomeSoaring(){
                     <div className={`border-b border-black-200 w-full h-14 flex`} key={index}>
                         <div className="w-10 h-12 font-extrabold text-gray-300 text-center mt-4 justify-center">{index + 1}</div>
                         <div className="w-12 h-12 border border-blue-500 rounded-full m-1" style={{background : `url(http://localhost:8080/estate_img/${item['Subscription.subscription_img_1']})`, backgroundSize : 'cover' }}></div>
-                        
                         <div className="mt-1">
-                            <div className="w-48 h-8 pt-1 font-bold">{item.real_estate_name}</div>
-                            <div className="w-48 h-4  text-xs font-semibold text-gray-500">{item.current_price}</div>
+                            <div className="w-36 h-8 pt-1 font-bold">{item.real_estate_name}</div>
+                            <div className="w-36 h-4  text-xs font-semibold  text-gray-500">현재가 : {item.current_price}</div>
+                        </div>
+                        <div className={` w-16 flex-col text-center mt-1`}>
+                            <div className='font-bold text-sm mt-1'>등락률</div>
+                            <div className={`
+                            ${item.fluctuation_rate > 0 ?
+                            'text-red-500'
+                            : item.fluctuation_rate < 0 ?
+                            'text-blue-500'
+                            : ""
+                            }
+                            `}>{item.fluctuation_rate}%</div>
                         </div>
                     </div>
                 );
--- userfrontend/src/features/HomeChart.tsx
+++ userfrontend/src/features/HomeChart.tsx
@@ -4,5 +4,6 @@ export type HomeChartType = {
     real_estate_name : string;
     current_price : number;
     fluctuation_rate : number;
+    dividend_price? : number
     'Subscription.subscription_img_1' : string;
 }
