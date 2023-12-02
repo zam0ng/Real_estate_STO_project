500656231c244c5a85526c274bb362b1f925f38c
killthecardz <alsdn8055@naver.com>
2023-12-03 00:28:41 +0900

    logo changed

--- userfrontend/src/pages/Login.tsx
+++ userfrontend/src/pages/Login.tsx
@@ -26,9 +26,9 @@ export default function Login() {
     <div className="animate-slide-up  w-full h-screen flex justify-center items-center flex-col ">
       <img src="/images/threeD/walking.png" className="h-3/4 animate-fadeIn"></img>
       <div className="text-center text-gray-800 text-bold  w-52 h-16 animate-fadeIn">
-        한걸음씩 ,건물주
+        한걸음씩 , 건물주
       </div>
-      <div className=" text-3xl animate-fadeIn mt-2 font-extrabold shadow-inner  ">BounSTO</div>
+      <div className=" text-3xl animate-fadeIn mt-2 font-extrabold  ">BounSTO</div>
     </div>
   );
 }
