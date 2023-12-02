9a01bdbee9c30768d0b67ffacd913f8705c285f6
killthecardz <alsdn8055@naver.com>
2023-12-02 14:27:55 +0900

    subscription detail page fixed

--- userfrontend/src/contents/subscription/subEnded/SubEndedList.tsx
+++ userfrontend/src/contents/subscription/subEnded/SubEndedList.tsx
@@ -39,17 +39,17 @@ export default function SubEndedList({ props }: SubAllListType) {
         
         {newData.map((building :SubAllList, index : number) => (
 
-        <div key={index} className='border-b-2 mx-2 h-12 m-auto mt-1 flex justify-between'>
-        <div className=" w-14 h-10 mr-2 mt-1 rounded-full" style={{background : `url('http://localhost:8080/estate_img/${building.subscription_img_1}')`, backgroundSize : 'cover'}}> </div>
-        <div className=" h-14 w-32 flex flex-col text-left pt-1  ">
+        <div key={index} className='border-b-2 mx-2 h-16 m-auto mt-1 flex justify-between'>
+        <div className=" w-16 h-9 mr-2 mt-3 rounded-full" style={{background : `url('http://localhost:8080/estate_img/${building.subscription_img_1}')`, backgroundSize : 'cover'}}> </div>
+        <div className=" h-14 w-40 flex flex-col text-left pt-3 mx-3  ">
             <span className="text-xs">{building.subscription_name}</span>
             <span className="text-xs pt-1">현재가{building.start_price}</span>
           </div>
-          <div className=" h-12 w-4/5 text-xxs  pt-2 flex-col  text-right whitespace-nowrap overflow-hidden ">
+          <div className=" h-12 w-4/5 text-xxs  pt-3 flex-col  text-right whitespace-nowrap overflow-hidden ">
             <div>{building.subscription_description}</div>
             <div>
               {building.start_price != null
-                ? <span>수익률  {(building.start_price - 5000) / 50 + "%"}</span>
+                ? <div className="mt-1">수익률  {(building.start_price - 5000) / 50 + "%"}</div>
                 : "청약 목표 미달성"}
             </div>
           </div>
