9f6cad7c5a1f7957fec94d0b097df4f1406f05b8
ojhhh <winnel@naver.com>
2023-12-03 17:11:25 +0900

    청약 상세 페이지 청약버튼 겹칩는거 수정

--- userfrontend/src/contents/subscription/subDetail/SubscriptionBtn.tsx
+++ userfrontend/src/contents/subscription/subDetail/SubscriptionBtn.tsx
@@ -14,12 +14,11 @@ type subdetailtype = {
   props: string | undefined;
 };
 
-export default function SubscriptionBtn({props} : subdetailtype){
-
-    let { buildingId } = useParams();
-    const [userBalance,setUserBalance] = useState(0);
-    const [quantity, setQuantity] = useState(0);
-    const [orderConfirm,setOrderConfirm] = useState(false);
+export default function SubscriptionBtn({ props }: subdetailtype) {
+  let { buildingId } = useParams();
+  const [userBalance, setUserBalance] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [orderConfirm, setOrderConfirm] = useState(false);
 
   const fetchData = async () => {
     const { data } = await axios.get(`/subscription/detail/${buildingId}`);
@@ -72,13 +71,11 @@ export default function SubscriptionBtn({props} : subdetailtype){
   }
 
   function handleQuantityBtn(num: number) {
-    
     setQuantity((prev) => {
-
-      console.log('이전값',prev)
-      console.log(typeof dataUserId)
-      console.log("dataUserId")
-      console.log(dataUserId)
+      console.log("이전값", prev);
+      console.log(typeof dataUserId);
+      console.log("dataUserId");
+      console.log(dataUserId);
       const newQuantity = prev + num;
       if (dataUserId < parseInt(detail.subscription_offering_price)) {
         setQuantity(0);
@@ -104,24 +101,25 @@ export default function SubscriptionBtn({props} : subdetailtype){
       console.log("newQuantity");
       console.log(newQuantity);
       return newQuantity;
-      
     });
   }
 
-        function handleQuantityInput(num : number){
+  function handleQuantityInput(num: number) {
+    const validNum = isNaN(num) ? 0 : num;
 
-            const validNum = isNaN(num) ? 0 : num;
-
-            if(validNum < 1){
-                setQuantity(0)
-            }
-            else if(validNum > Math.floor( dataUserId / parseInt(detail.subscription_offering_price) )){
-
-                setQuantity(Math.floor(dataUserId / parseInt(detail.subscription_offering_price)));
-            }else{
-                setQuantity(validNum)
-            }
-        }
+    if (validNum < 1) {
+      setQuantity(0);
+    } else if (
+      validNum >
+      Math.floor(dataUserId / parseInt(detail.subscription_offering_price))
+    ) {
+      setQuantity(
+        Math.floor(dataUserId / parseInt(detail.subscription_offering_price))
+      );
+    } else {
+      setQuantity(validNum);
+    }
+  }
 
   function handleOrder() {
     setOrderConfirm(true);
@@ -139,52 +137,78 @@ export default function SubscriptionBtn({props} : subdetailtype){
     return `${amount.toLocaleString("ko-KR")}`;
   }
 
+  return (
+    <>
+      {orderConfirm ? (
+        <OrderConfirm
+          setOrderConfirm={setOrderConfirm}
+          dataUserId={dataUserId}
+          dataSubDetail={dataSubDetail}
+          quantity={quantity}
+        />
+      ) : null}
 
-
-
-    return(
-      <>
-        {orderConfirm ? <OrderConfirm setOrderConfirm ={setOrderConfirm} dataUserId = {dataUserId} dataSubDetail = {dataSubDetail} quantity={quantity} />: null}
-        
-        {isCookie 
-        ? 
-          <>
+      {isCookie ? (
+        <>
           <div className=" w-full h-full border z-10  m-auto fixed top-96 shadow-lg bg-white bg-gradient-to-brounded-3xl animate-slide-up rounded-2xl ">
-            <RiArrowDropDownLine  className="mt-4 ml-4 w-6 h-6 m-auto" onClick={()=>setIsCookie(false)}/>
+            <RiArrowDropDownLine
+              className="mt-4 ml-4 w-6 h-6 m-auto"
+              onClick={() => setIsCookie(false)}
+            />
             <div className="w-5/6  m-auto text-right  h-6 mb-5 text-sm">
-                <span className="bg-gray-100 text-blue-400 rounded-sm px-2 py-1 font-bold">
+              <span className="bg-gray-100 text-blue-400 rounded-sm px-2 py-1 font-bold">
                 잔고 : {formatCurrency(dataUserId)}
                 {/* <span className="ml-1"></span> */}
-                </span>
+              </span>
             </div>
             <div className=" text-right h-14 w-5/6 m-auto flex justify-between mb-5">
-                <div>
-                    <input type="number" className=" w-8 mt-3 font-extrabold text-center" placeholder="0" value={quantity} onChange={(event)=>handleQuantityInput(parseInt(event?.target.value))}></input><span className="text-xs">주</span>
+              <div>
+                <input
+                  type="number"
+                  className=" w-8 mt-3 font-extrabold text-center"
+                  placeholder="0"
+                  value={quantity}
+                  onChange={(event) =>
+                    handleQuantityInput(parseInt(event?.target.value))
+                  }
+                ></input>
+                <span className="text-xs">주</span>
+              </div>
+              <div className="w-28  ">
+                <div className="inline-block border border-gray-200 w-12 h-12 text-center text-gray-400 hover:text-blue-400 ">
+                  <button
+                    className="h-full "
+                    onClick={() => handleQuantityBtn(-1)}
+                  >
+                    <FaMinus className=" " />
+                  </button>
                 </div>
-                <div className="w-28  ">
-                    <div className="inline-block border border-gray-200 w-12 h-12 text-center text-gray-400 hover:text-blue-400 ">
-                        <button className="h-full " onClick={()=>handleQuantityBtn(-1)} >
-                        <FaMinus className=" " />
-                        </button>
-                    </div>
-                    <div className="inline-block border border-gray-200 w-12 h-12 text-center text-gray-400 hover:text-blue-400">
-                        <button className="h-full " onClick={()=>handleQuantityBtn(+1)}>
-                        <FaPlus className=""/>
-                        </button>
-                    </div>
+                <div className="inline-block border border-gray-200 w-12 h-12 text-center text-gray-400 hover:text-blue-400">
+                  <button
+                    className="h-full "
+                    onClick={() => handleQuantityBtn(+1)}
+                  >
+                    <FaPlus className="" />
+                  </button>
                 </div>
+              </div>
             </div>
             <div className="w-5/6 m-auto h-9  font-extrabold flex justify-between">
-                <div>공모가</div>
-                <div>5,000 원</div>
+              <div>공모가</div>
+              <div>5,000 원</div>
             </div>
             <div className="w-5/6 m-auto h-9 font-extrabold flex justify-between">
-                <div>구매수량</div>
-                <div className="w-20 text-right">{formatCurrency(quantity)}</div>
+              <div>구매수량</div>
+              <div className="w-20 text-right">{formatCurrency(quantity)}</div>
             </div>
             <div className="w-5/6 m-auto h-9 border-t-2 border-black font-extrabold flex justify-between pt-3">
-                <div className="text-blue-500">총 청약 금액</div>
-                <div className="w-25 text-blue-500 text-right">{formatCurrency((quantity) * parseInt(detail.subscription_offering_price)) }<span className="ml-1">원</span></div>
+              <div className="text-blue-500">총 청약 금액</div>
+              <div className="w-25 text-blue-500 text-right">
+                {formatCurrency(
+                  quantity * parseInt(detail.subscription_offering_price)
+                )}
+                <span className="ml-1">원</span>
+              </div>
             </div>
             <div
               className={` w-5/6 h-12 rounded-md ${
@@ -195,15 +219,15 @@ export default function SubscriptionBtn({props} : subdetailtype){
               주문하기
             </div>
           </div>
-          </>
-       : 
+        </>
+      ) : (
         <div
           className=" w-5/6 h-12 rounded-md bg-blue-950 text-white m-auto flex justify-center items-center font-semibold my-4"
           onClick={() => handleSubscription()}
         >
           청약하기
         </div>
-        }
-      </>
+      )}
+    </>
   );
 }
