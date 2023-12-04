592a004c52b1eb1ab2d9c8aa747714603c79782e
zam0ng <sinze27@gmail.com>
2023-12-04 14:20:20 +0900

    edit

--- backend/app.ts
+++ backend/app.ts
@@ -63,7 +63,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://bouncesto.site"],
+    origin: ["http://localhost:3000", "https://bouncesto.site", "https://bs.admin.bouncesto.site"],
     credentials: true,
   })
 );
